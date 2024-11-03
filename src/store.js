c918d9ebc8989a8e7315bc99b2c54e867840ad6b
qoryhanisagal <sequoyah.m.dozier@gmail.com>
2024-11-03 14:14:01 -0500

    Task 5 complete

--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+import cartReducer from './CartSlice'; // Import the cartReducer from CartSlice
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer, // Assign cartReducer to manage the 'cart' slice of state
+  },
 });
-export default store
+export default store; // Export the store for use in the application
